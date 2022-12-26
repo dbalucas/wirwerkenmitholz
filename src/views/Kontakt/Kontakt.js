63e6db3b4e47054447a1c73dea7a95e0774ab282
dbalucas <lucas.duerksen.work@gmail.com>
2022-12-26 11:29:36 +0100

    Optimierungen für das Formular mit Formik

--- src/views/Kontakt/Kontakt.js
+++ src/views/Kontakt/Kontakt.js
@@ -1,50 +1,56 @@
-import React, { useState } from "react";
+import React from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 
-const Kontakt = () => {
-  return (
-    <Formik
-      initialValues={{ name: '', email: '', message: '' }}
-      validate={values => {
-        const errors = {};
-        if (!values.name) {
-          errors.name = 'Required';
-        }
-        if (!values.email) {
-          errors.email = 'Required';
-        } else if (
-          !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-        ) {
-          errors.email = 'Invalid email address';
-        }
-        if (!values.message) {
-          errors.message = 'Required';
-        }
-        return errors;
-      }}
-      onSubmit={(values, { setSubmitting }) => {
-        setTimeout(() => {
-          alert(JSON.stringify(values, null, 2));
-          setSubmitting(false);
-        }, 400);
-      }}
-    >
-      {({ isSubmitting }) => (
-        <Form>
-          <Field type="text" name="name" />
-          <ErrorMessage name="name" component="div" />
-          <Field type="email" name="email" />
-          <ErrorMessage name="email" component="div" />
-          <Field component="textarea" name="message" />
-          <ErrorMessage name="message" component="div" />
-          <button type="submit" disabled={isSubmitting}>
-            Submit
-          </button>
-        </Form>
-      )}
-    </Formik>
-  );
+const initialValues = {
+  name: 'Name',
+  email: 'E-Mail-Adresse',
+  message: 'hier bitte die Nachricht eintragen',
 };
+
+const validate = values => {
+  const errors = {};
+  if (!values.name) {
+    errors.name = 'Required';
+  }
+  if (!values.email) {
+    errors.email = 'Required';
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+    errors.email = 'Invalid email address';
+  }
+  if (!values.message) {
+    errors.message = 'Required';
+  }
+  return errors;
+};
+
+const handleSubmit = async (values, { setSubmitting }) => {
+  await new Promise(resolve => setTimeout(resolve, 400));
+  alert(JSON.stringify(values, null, 2));
+  setSubmitting(false);
+};
+const MyForm = ({ isSubmitting }) => (
+  <Form style={{ display: 'flex', flexDirection: 'column'}}>
+    <Field type="text" name="name" />
+    <ErrorMessage name="name" component="div" />
+    <Field type="email" name="email" />
+    <ErrorMessage name="email" component="div" />
+    <Field component="textarea" name="message" />
+    <ErrorMessage name="message" component="div" />
+    <button type="submit" disabled={isSubmitting}>
+      Submit
+    </button>
+  </Form>
+);
+
+const Kontakt = () => (
+  <Formik
+    initialValues={initialValues}
+    validate={validate}
+    onSubmit={handleSubmit}
+  >
+    {({ isSubmitting }) => <MyForm isSubmitting={isSubmitting} />}
+  </Formik>
+);
 
 
 export default Kontakt;
